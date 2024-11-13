7c8e22ca2076cceb255a68f7446cd88a2e8b41b9
Renks <renks@localhost.localdomain>
2024-11-13 13:50:36 -0300

    change: get_properties needs page number param

--- scod/modules/property.py
+++ scod/modules/property.py
@@ -1,5 +1,3 @@
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import cpu_count
 from typing import Union, List, Dict
 from .validators import Validators
 import requests
@@ -16,20 +14,13 @@ class Property(Validators):
 
         return data
 
-    def get_properties(self, from_page: int, to_page: int) -> Union[List[Dict[str, str]], List[None]]:
+    def get_properties(self, page: int) -> Union[List[Dict[str, str]], List[None]]:
 
-        def get_properties(page: int) -> Union[List[Dict[str, str]], List[None]]:
-            response = requests.get("https://api.scod.com.br/v3/imovel/listar/imoveis/",
-                                    headers=self._Scod__headers, params={"pagina": page})
+        response = requests.get("https://api.scod.com.br/v3/imovel/listar/imoveis",
+                                headers=self._Scod__headers, params={"pagina": page})
 
-            data = self._Validators__response_validator(response)
-
-            return data
-
-        with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
-            data = list(executor.map(get_properties, range(from_page, to_page + 1)))
-            data = [item for sublist in data for item in sublist]
-            return data
+        data = self._Validators__response_validator(response)
+        return data
 
     def get_property_info(self, id: str) -> Union[List[Dict[str, str]], List[None]]:
         response = requests.get(f"https://api.scod.com.br/v3/imovel/iptu/dados/{id}",
@@ -42,11 +33,12 @@ class Property(Validators):
 
         return data
 
-    def get_debt_file(self, property_id: str, debt_id: str) -> Union[List[Dict[str, str]], List[None]]:
+    def get_debt_file(self, property_id: str, debt_id: str, timeout=30) -> Union[List[Dict[str, str]], List[None]]:
         response = requests.post(f"https://api.scod.com.br/v3/imovel/iptu/boleto/{property_id}",
-                                 headers=self._Scod__headers, json=[debt_id])
+                                headers=self._Scod__headers, json=[debt_id], timeout=timeout)
 
         data = self._Validators__response_validator(response)
+
         if 'error' in data and data['error']:
             return data['mensagem']
 
